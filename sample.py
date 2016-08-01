7bab882b8bf5777ca1a5f1f67e67d48c539f55a6
Zach <zach@Zacharys-MBP.home>
2016-08-01 08:19:40 -0400

    Added Mobile and No Mobile functions

--- sample.py
+++ sample.py
@@ -1,16 +1,7 @@
-f_read = open("Data/DB24-IP-COUNTRY-REGION-CITY-LATITUDE-LONGITUDE-ZIPCODE-TIMEZONE-ISP-DOMAIN-NETSPEED-AREACODE-WEATHER-MOBILE-ELEVATION-USAGETYPE.CSV/IP-COUNTRY-REGION-CITY-LATITUDE-LONGITUDE-ZIPCODE-TIMEZONE-ISP-DOMAIN-NETSPEED-AREACODE-WEATHER-MOBILE-ELEVATION-USAGETYPE.CSV")
-
-f_write = open("data_7.csv","w")
-count = 0
-
-for line in f_read:
-    
-	if count == 14000000:
-		break
-	elif count >= 12000000:    
-		f_write.write(line)
-        
-	count = count + 1
-
-f_read.close()
-f_write.close()
+o = open("Data/Split/data_8.csv","w")
+for n in range(2,8):
+	f = open("Data/Split/data_"+str(n)+".csv","r")
+	s = f.readline()
+	o.write(s)
+	f.close()
+o.close()
